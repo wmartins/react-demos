74f69c5a5d81d37e9ada6bf3ae01cdd9f2755526
William Martins <wwwhmartins@gmail.com>
2015-08-27 11:32:10 -0300

    Add playground

---
+++ 00 - Playground/src/scripts/app.jsx.js
@@ -0,0 +1,9 @@
+var React      = require('react'),
+    appDOMNode = document.querySelector('#js-react-app');
+
+React.render(
+    <div className="App">
+        Init =)
+    </div>,
+    appDOMNode
+);
